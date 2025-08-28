daf31803e6997fac57d3fbf6ecdda7ff7bb72864
Matthew Aitken <matt@itken.net>
2025-08-28 15:13:51 +0100

    Add loader and actions

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import Posts from './routes/Posts'
-import './index.css'
-import NewPost from './routes/NewPost'
+import Posts, { loader as postsLoader } from './routes/Posts'
+import PostDetails, { loader as postDetailsLoader } from './routes/PostDetails'
+import NewPost, { action as newPostAction } from './routes/NewPost'
 import RootLayout from './routes/RootLayout'
+import './index.css'
 
 const router = createBrowserRouter([
   {
@@ -15,8 +16,10 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Posts />,
+        loader: postsLoader,
         children: [
-          { path: '/create-post', element: <NewPost /> }
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
+          { path: '/:id', element: <PostDetails />, loader: postDetailsLoader }
         ]
       },
     ]
--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,47 +1,39 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Form, redirect } from 'react-router-dom';
 
 import classes from './NewPost.module.css'
 import Modal from '../components/Modal';
 
-export default function NewPost({ onAddPost }) {
-    const [bodyText, setBodyText] = useState('');
-    const [authorText, setAuthorText] = useState('');
-
-    function handleBodyChange(event) {
-        setBodyText(event.target.value);
-    }
-
-    function handleAuthorChange(event) {
-        setAuthorText(event.target.value);
-    }
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        const postData = {
-            id: authorText + Math.random(),
-            body: bodyText,
-            author: authorText
-        };
-        onAddPost(postData);
-    }
-
+export default function NewPost() {
     return (
         <Modal>
-            <form className={classes.form} onSubmit={handleSubmit}>
+            <Form method='POST' className={classes.form}>
                 <p>
                     <label htmlFor='body'>Text</label>
-                    <textarea id='body' required rows={3} onChange={handleBodyChange} />
+                    <textarea id='body' name='body' required rows={3} />
                 </p>
                 <p>
                     <label htmlFor='name'>Your name</label>
-                    <input id='name' required onChange={handleAuthorChange} />
+                    <input id='name' name='author' required />
                 </p>
                 <p className={classes.actions}>
                     <Link to='..' type='button'>Cancel</Link>
                     <button>Submit</button>
                 </p>
-            </form>
+            </Form>
         </Modal>
     )
+}
+
+export async function action({ request }) {
+    const formData = await request.formData();
+    const postData = Object.fromEntries(formData);
+    await fetch('http://localhost:8080/posts', {
+        method: 'POST',
+        body: JSON.stringify(postData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    return redirect('/');
 }
